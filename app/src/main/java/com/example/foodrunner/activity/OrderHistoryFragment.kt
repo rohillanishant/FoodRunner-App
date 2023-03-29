d60f3ad518346b6b1316be51f1e75a393532a3e4
Nishant <rohillanishant19@gmail.com>
2023-03-29 20:29:50 +0530

    Payment Gateway added

--- app/src/main/java/com/example/foodrunner/activity/OrderHistoryFragment.kt
+++ app/src/main/java/com/example/foodrunner/activity/OrderHistoryFragment.kt
@@ -40,7 +40,6 @@ class OrderHistoryFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view=inflater.inflate(R.layout.fragment_order_history, container, false)
         recyclerOrder=view.findViewById(R.id.recyclerOrder)
         layoutManager= LinearLayoutManager(activity)
